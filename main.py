e2ffce325693235f95c43a0f053bffeaa7d543fc
Orion-himiko <prishah.shah0707@gmail.com>
2024-12-23 21:33:24 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+english=int(input("English marks: "))
+math=int(input("Math marks: "))
+chem=int(input("Chem marks: "))
+bio=int(input("Bio marks: "))
+french=int(input("French marks: "))
+
+sum=english+math+chem+bio+french
+
+total=(sum/500)*100
+print("Total Percentage:",total)
